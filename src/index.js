4f68f4209f0ca51b956c02dc22ccd09404059b47
Harvey <harvey0379@163.com>
2020-04-10 09:53:25 +0800

    :sparkles: style-loader & css-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import foo from './foo';
+require('!style-loader!css-loader!./assets/index.css');
 
 const div = document.createElement('DIV');
 div.innerHTML = _.join(['Hello', 'World', foo.name], ' ');
